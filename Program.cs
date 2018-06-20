19aa29966bc4b6bd1b0f90d389a10d1b2b55f15b
Keith Fletcher <keith@fletcher.org>
2018-06-20 18:47:52 +1200

    A few tweaks

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ namespace GpxParser
                     .Select(pt => pt.ToXml()));
                 
 
-            "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><gpx version=\"1.0\" creator="GPSLogger 95 - http://gpslogger.mendhak.com/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/0" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd"><trk><trkseg>
+            // "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><gpx version=\"1.0\" creator="GPSLogger 95 - http://gpslogger.mendhak.com/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.topografix.com/GPX/1/0" xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd"><trk><trkseg>
 
             Console.WriteLine();
         }
